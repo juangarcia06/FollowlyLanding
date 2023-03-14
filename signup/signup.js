7b5e967f13a7f4b7e52d93ecdb085ff67a79ae6e
BananaC9 <superjuan.madrid2015@gmail.com>
2023-03-14 23:16:47 +0100

    fix signup

--- signup/signup.js
+++ signup/signup.js
@@ -3,21 +3,17 @@ async function handleSignUp() {
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
     const response = await fetch('http://localhost:5000/create-account', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ username, email, password }),
-    });
-    const data = await response.text();
-    if (data === 'Account created') {
-      // signup was successful, update the app state to show the Account page
-      alert('Account created');
-      // update state or redirect to a new page here
-    } else {
-      // signup was not successful, show an error message to the user
-      alert('Error');
-    }
-    alert(username + password + email);
-  }
-  
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, email, password }),
+    }.then((response) => {
+        if (response.ok) {
+            window.location.replace('https://app.followly.es');
+        } else {
+            return response.json();
+        }
+    })    
+    );
+}
